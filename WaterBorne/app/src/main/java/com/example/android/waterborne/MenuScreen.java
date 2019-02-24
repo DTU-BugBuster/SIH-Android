920103bf6416f89c41d3c2544fca818610b4cd74
Dhruv Jain <dhruvjain2212000@gmail.com>
2019-02-24 19:19:14 +0530

    IsPlaceSafe kashmir poora nahi hai par :(

--- WaterBorne/app/src/main/java/com/example/android/waterborne/MenuScreen.java
+++ WaterBorne/app/src/main/java/com/example/android/waterborne/MenuScreen.java
@@ -19,6 +19,7 @@ import com.example.android.waterborne.Auth.LoginActivity;
 import com.example.android.waterborne.ChatApp.AnonymousChat;
 import com.example.android.waterborne.ChatBotRelated.ChatbotActivity;
 import com.example.android.waterborne.HomeRemedies.HomeRemedy;
+import com.example.android.waterborne.IsPlaceSafe.IsPlaceSafeActivity;
 import com.example.android.waterborne.Models.Item;
 import com.example.android.waterborne.NearbyHospitalsRelated.NearbyHospitalsActivity;
 import com.google.firebase.auth.FirebaseAuth;
@@ -247,7 +248,7 @@ public class MenuScreen extends AppCompatActivity implements View.OnClickListene
             startActivity(new Intent(getBaseContext(),NewsActivity.class));
             Toast.makeText(this, "Will add in next version", Toast.LENGTH_SHORT).show();
         } else if (item.getText().equals("Is Place Safe")) {
-//            startActivity(new Intent(getBaseContext(),placeSafe.class));
+            startActivity(new Intent(getBaseContext(),IsPlaceSafeActivity.class));
             Toast.makeText(this, "Don't blame me ask pranav xD", Toast.LENGTH_SHORT).show();
         } else if (item.getText().equals("Home Remedies")) {
             startActivity(new Intent(getBaseContext(),HomeRemedy.class));
